f3d80ae72687b84a894a3fcd18157f7318774d62
Himanshu Upadhyay <43202280+me2140733@users.noreply.github.com>
2021-10-24 09:40:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,36 +15,39 @@ def home():
 
 @app.route("/predict", methods = ["GET", "POST"])
 @cross_origin()
+
 def predict():
     if request.method == "POST":
 
         # Total Stops
         Age = int(request.form["Age"])
-        Sex = int(request.form["Sex"])
-        eGFR = int(request.form["eGFR(mL/min/1.73 m^2)"])
-        Hypoalbuminemia = int(request.form["Hypoalbuminemia"])
-        Duration = (int(request.form["Expected surgical Duration(hours)"]))*5
-        Emergency = int(request.form["emergency operation"])
-        Diabetes = int(request.form["Diabetes"])
-        RAAS = int(request.form["RAAS Blockade Use"])
-        Anemia = int(request.form["Anemia"])
-        Hyponatremia = int(request.form["Hyponatremia"])
+        Gender = int(request.form["Gender"])
+        Total_Bilirubin = int(request.form["Total Bilirubin"])
+        Direct_Bilirubin = int(request.form["Direct Bilirubin"])
+        Alkaline_Phosphotase = int(request.form["Alkaline Phosphotase"]
+        Alamine_Aminotransferase = int(request.form["Alamine Aminotransferase"])
+        Aspartate_Aminotransferase = int(request.form["Aspartate Aminotransferase"])
+        Total_Protiens = int(request.form["Total Protiens"])
+        Albumin = int(request.form["Albumin"])
+        Albumin_and_Globulin_Ratio = int(request.form["Albumin and Globulin Ratio"])
         # print(Total_stops)
+        loaded_model = pickle.load(open("LiverDisease.pkl","rb"))
+        data= [[Age,Gender,Total_Bilirubin,Direct_Bilirubin,Alkaline_Phosphotase,Alamine_Aminotransferase,
+               Aspartate_Aminotransferase,Total_Protiens,Albumin,Albumin_and_Globulin_Ratio]]
+        dftest = pd.DataFrame(data, columns = ['Age','Gender','Total_Bilirubin','Direct_Bilirubin','Alkaline_Phosphotase',
+                                               'Alamine_Aminotransferase','Aspartate_Aminotransferase','Total_Protiens','Albumin','Albumin_and_Globulin_Ratio'])                           
+        result = loaded_model.predict(dftest)
+                             
         
-        score = Age + Sex + eGFR + Hypoalbuminemia + Duration + Emergency + Diabetes + RAAS + Anemia + Hyponatremia
 
-        if(score<20):
+   
 
-            return render_template('home.html',prediction_text="This patient has AKI risk < 2% and critical AKI risk < 2%")
-        if(20<=score<=39):
+        if(result[0]==1):
 
-            return render_template('home.html',prediction_text="This patient has AKI risk >= 2% and critical AKI risk < 2%")
-        if(40<=score<=59):
+            return render_template('home.html',prediction_text="This patient may have liver disease")
+        if(result[0]==0):
 
-            return render_template('home.html',prediction_text="This patient has AKI risk >= 10% and critical AKI risk >= 2%")
-        if(score>=60):
-
-            return render_template('home.html',prediction_text="This patient has AKI risk >= 20% and critical AKI risk >= 10%")
+            return render_template('home.html',prediction_text="This patient may not have liver disease")
 
 
     return render_template("home.html")
